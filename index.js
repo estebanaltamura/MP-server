d8a474f7d7e06036f21b1ad314994d2796f33b41
estebanaltamura <esteban.altamura@gmail.com>
2023-08-19 12:01:47 -0300

    actualizacion

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-const options = {
-  key: fs.readFileSync('/etc/cert/privkey.pem'),
-  cert: fs.readFileSync('/etc/cert/cert.pem')
-};
-
 app.use(express.json());
 app.use(cors());
 
@@ -26,18 +21,13 @@ app.post('/createOrder', (req, res) => {
   res.status(200)
   console.log("body", req.body.items)
 
-  const items = req.body.items
-  
+  const items = req.body.items  
   const itemsHandled = items.map((item)=>({"id": Math.round(Math.random(),2), "title": item.name, "unit_price": item.price, "quantity": item.quantity, currency_id: "ARS", "picture_url": item.images[0]}))
-
-  console.log(itemsHandled)
-
-  // Agrega credenciales
+  
   mercadopago.configure({
-    access_token: 'APP_USR-6329496204948801-071110-eb8bf56f043b2dea1ae0b8b8a3b04833-1421065886'
+    access_token: 'TEST-3355990679256381-071110-38d56cd52fb2a05c5f934b421e419c33-13941450'
   });
-
-  // Crea un objeto de preferencia
+  
   let preference = { 
     "back_urls": {
       "success": "https://entrega-final-altamura.vercel.app/",
@@ -61,6 +51,11 @@ app.post('/createOrder', (req, res) => {
       res.sendStatus(500);
     });
 });
+
+const options = {
+  key: fs.readFileSync('/etc/cert/privkey.pem'),
+  cert: fs.readFileSync('/etc/cert/cert.pem')
+};
 
 const server = https.createServer(options, app);
 
