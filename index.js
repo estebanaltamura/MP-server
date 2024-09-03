3791928a9d0cf7fc6d63257909bb7ed62dfdffaf
Andres Altamura <esteban.altamura@gmail.com>
2024-09-03 09:31:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mercadopago = require('mercadopago');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port = 5100;
 
 app.use(express.json());
 app.use(cors());
